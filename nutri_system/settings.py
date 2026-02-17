3643c7209c2125e340b655816e8a05a82316d478
Marcos Felipe Amorim Mendes <nutriamorim69@gmail.com>
2026-02-17 13:06:54 -0300

    ajustando banco para local e produção

--- nutri_system/settings.py
+++ nutri_system/settings.py
@@ -63,11 +63,19 @@ WSGI_APPLICATION = 'nutri_system.wsgi.application'
 
 
 # Database
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
-}
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.environ.get("DATABASE_URL")
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 
 
 # Password validation
